68952c05a03de0f8b337d9a2c8f320b5dd54d534
Anna Leshcheva <dediaevanna@gmail.com>
2024-05-18 19:27:15 -0500

    подготовили к публикации

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,65 @@
-const gulp = require('gulp'); 
+const gulp = require('gulp');
 const concat = require('gulp-concat-css');
 const plumber = require('gulp-plumber');
 const del = require('del');
 const browserSync = require('browser-sync').create();
 
 function html() {
-    return gulp.src('src/**/*.html')
-          .pipe(plumber())
-                  .pipe(gulp.dest('dist/'))
-          .pipe(browserSync.reload({stream: true}));
-  }
-  
-  exports.html = html;
+  return gulp
+    .src('src/**/*.html')
+    .pipe(plumber())
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
+}
 
-  function css() {
-    return gulp.src('src/blocks/**/*.css')
-          .pipe(plumber())
-          .pipe(concat('bundle.css'))
-                  .pipe(gulp.dest('dist/'))
-          .pipe(browserSync.reload({stream: true}));
-  }
-  
-  exports.css = css;
+exports.html = html;
 
-  function images() {
-    return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
-              .pipe(gulp.dest('dist/images'))
-              .pipe(browserSync.reload({stream: true}));
-  }
-  
-  exports.images = images;
+function css() {
+  return gulp
+    .src('src/blocks/**/*.css')
+    .pipe(plumber())
+    .pipe(concat('bundle.css'))
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
+}
 
-  function clean() {
-    return del('dist');
-  }
-  
-  exports.clean = clean;
+exports.css = css;
 
-  const build = gulp.series(clean, gulp.parallel(html, css, images));
+function images() {
+  return gulp
+    .src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}', {
+      removeBOM: false,
+    })
+    .pipe(gulp.dest('dist/images'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+exports.images = images;
+
+function clean() {
+  return del('dist');
+}
+
+exports.clean = clean;
+
+const build = gulp.series(clean, gulp.parallel(html, css, images));
 
 exports.build = build;
 
 function watchFiles() {
-    gulp.watch(['src/**/*.html'], html);
-    gulp.watch(['src/blocks/**/*.css'], css);
-    gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
-  }
-  const watchapp = gulp.parallel(build, watchFiles, serve);
-  
-  exports.watchapp = watchapp;
-  exports.default = watchapp;
+  gulp.watch(['src/**/*.html'], html);
+  gulp.watch(['src/blocks/**/*.css'], css);
+  gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+}
+const watchapp = gulp.parallel(build, watchFiles, serve);
 
-  function serve() {
-    browserSync.init({
-      server: {
-        baseDir: './dist'
-      }
-    });
-  }
+exports.watchapp = watchapp;
+exports.default = watchapp;
+
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: './dist',
+    },
+  });
+}
