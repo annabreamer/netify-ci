d6adac7007a8c76d83bd35b77c93fd58aa2b4059
Anna Leshcheva <dediaevanna@gmail.com>
2024-05-20 19:28:39 -0500

    подготовили к публикации

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,33 @@ const concat = require('gulp-concat-css');
 const plumber = require('gulp-plumber');
 const del = require('del');
 const browserSync = require('browser-sync').create();
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const mediaquery = require('postcss-combine-media-query');
+const cssnano = require('cssnano');
+const htmlMinify = require('html-minifier');
 
 function html() {
+  const options = {
+    removeComments: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    sortClassName: true,
+    useShortDoctype: true,
+    collapseWhitespace: true,
+    minifyCSS: true,
+    keepClosingSlash: true,
+  };
   return gulp
     .src('src/**/*.html')
     .pipe(plumber())
+    .on('data', function (file) {
+      const buferFile = Buffer.from(
+        htmlMinify.minify(file.contents.toString(), options)
+      );
+      return (file.contents = buferFile);
+    })
     .pipe(gulp.dest('dist/'))
     .pipe(browserSync.reload({ stream: true }));
 }
@@ -15,10 +37,12 @@ function html() {
 exports.html = html;
 
 function css() {
+  const plugins = [autoprefixer(), mediaquery(), cssnano()];
   return gulp
     .src('src/blocks/**/*.css')
     .pipe(plumber())
     .pipe(concat('bundle.css'))
+    .pipe(postcss(plugins))
     .pipe(gulp.dest('dist/'))
     .pipe(browserSync.reload({ stream: true }));
 }
@@ -51,7 +75,7 @@ function watchFiles() {
   gulp.watch(['src/blocks/**/*.css'], css);
   gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
 }
-const watchapp = gulp.parallel(build, watchFiles, serve);
+const watchapp = gulp.series(build, gulp.parallel(watchFiles, serve));
 
 exports.watchapp = watchapp;
 exports.default = watchapp;
